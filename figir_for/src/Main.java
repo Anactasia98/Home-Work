190ffcf1ab5debc8a85010cbcef983cd67c1e5e2
Anactasia98 <162879082+Anactasia98@users.noreply.github.com>
2024-11-09 13:10:42 +0300

    for figir

---
+++ figir_for/src/Main.java
@@ -0,0 +1,105 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Прямоугольник");
+        int star=7;
+        for (int i = 0; i <star ; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(" *" );
+            }
+            System.out.println();
+        }
+        System.out.println("Треугольик с прямым углом");
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("Перевернутый треугольик с прямым углом");
+
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j <star-i; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("косая линия вниз");
+
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        System.out.println("косая линия вверх");
+
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j <star-i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+        System.out.println("треугольник");
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j <star-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<i+1; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("Ромб");
+
+        for (int i = 0; i < star; i++) {
+            for (int j = 0; j <star-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j<i+1; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+        for (int i =0; i<star-1; i++) {
+            for (int j = 0; j <i+2 ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <(star-1)-i ; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
