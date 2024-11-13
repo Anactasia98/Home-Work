a7cb9e1609165767ec0cdf2490eb5187c42babb8
Anactasia98 <162879082+Anactasia98@users.noreply.github.com>
2024-11-13 16:08:24 +0300

    undate

---
+++ Arrays/src/Main.java
@@ -0,0 +1,131 @@
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        1) Создайте массив из 10 целых чисел и заполните его числами от 1 до 10. Выведите массив на экран.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//        for (int i=0;i< arr.length;i++){
+//            System.out.print(arr[i] + " ");
+//        }
+
+//        2) Найдите сумму элементов массива. Создайте массив из 5 элементов и вычислите сумму всех чисел в массиве.
+//        int []arr={1,2,3,4,5};
+//       int sum=0;
+//        for (int i=0;i< arr.length;i++){
+//            sum+=arr[i];
+//        }
+//        System.out.print(sum);
+
+//        3) Найдите максимальный элемент массива. Создайте массив из 10 целых чисел и найдите максимальное значение.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//       int max=arr[0];
+//        for (int i=0;i< arr.length;i++){
+//           if (arr[i]>max){
+//               max=arr[i];
+//           }
+//        }
+//        System.out.print("Max=" + max);
+
+//        4) Найдите минимальный элемент массива. Создайте массив из 10 целых чисел и найдите минимальное значение.
+//        int []arr={1,2,3,4,-5,6,7,8,9,10};
+//        int min=arr[0];
+//        for (int i=0;i< arr.length;i++){
+//            if (arr[i]<min){
+//                min=arr[i];
+//            }
+//        }
+//        System.out.print("Min=" + min);
+
+//        5) Посчитайте количество чётных чисел в массиве.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//        int count=0;
+//        for (int i=0;i< arr.length;i++){
+//            if (arr[i]%2==0){
+//                count++;
+//            }
+//        }
+//        System.out.print("Количество чётных чисел " + count);
+
+//        6) Посчитайте количество нечётных чисел в массиве.
+//        int []arr={1,2,3,4,5,6,7,8,8,10};
+//        int count=0;
+//        for (int i=0;i< arr.length;i++){
+//            if (arr[i]%2!=0){
+//                count++;
+//            }
+//        }
+//        System.out.print("Количество чётных чисел " + count);
+
+//        7) Посчитайте среднее арифметическое всех элементов массива.
+//        int []arr={1,2,3,4,5};
+//        int arifmetic=0;
+//        int sum=0;
+//        for (int i=0;i< arr.length;i++){
+//            sum+=arr[i];
+//            arifmetic=sum/ arr.length;
+//        }
+//        System.out.print("среднее арифметическое " + arifmetic);
+
+//        8) Выведите все элементы массива в обратном порядке.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//        for (int i= arr.length-1;i>=0;i--){
+//            System.out.print(arr[i] + " ");
+//        }
+
+//        9) Заполните массив случайными числами от 1 до 100 и выведите его на экран.
+//        int []arr=new int[20];
+//        Random random=new Random();
+//        for (int i = 0; i < arr.length; i++) {
+//            arr[i]= random.nextInt(1,101);
+//            System.out.print(arr[i] + " ");
+//        }
+//        10)Поменяйте местами первый и последний элементы массива.
+//        int []arr={1,2,3,4,5,6,7,8,98,10};
+//        int k=arr[0];
+//        for (int i=0;i< arr.length;i++){
+//            arr[0]= arr[arr.length-1];
+//           arr[arr.length-1]=k;
+//            System.out.print(arr[i] + " ");
+//        }
+
+//        11)Найдите разницу между максимальным и минимальным элементами массива.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//       int max=arr[0];
+//       int min=arr[0];
+//        for (int i=0;i< arr.length;i++){
+//           if (arr[i]>max){
+//               max=arr[i];
+//           }
+//           if (arr[i]<min){
+//               min=arr[i];
+//           }
+//        }
+//        System.out.print("разницу между максимальным и минимальным=" + (max-min));
+
+//        12)Посчитайте количество элементов, больше среднего арифметического.
+//        int []arr={1,2,3,4,5};
+//        int arifmetic=0;
+//        int sum=0;
+//        int count=0;
+//        for (int i=0;i< arr.length;i++){
+//            sum+=arr[i];
+//            arifmetic=sum/ arr.length;
+//        }
+//        for (int i = 0; i < arr.length ; i++) {
+//            if (arifmetic<arr[i]){
+//                count++;
+//            }
+//        }
+//        System.out.println("количество элементов, больше среднего арифметического: " + count);
+
+//        13)Создайте массив и заполните его квадратами чисел от 1 до 10.
+//        int []arr={1,2,3,4,5,6,7,8,9,10};
+//        for (int i=0;i< arr.length;i++){
+//           arr[i]=arr[i]*arr[i];
+//            System.out.print(arr[i] + " ");
+//        }
+
+        }
+    }
